f0353eb893479146070fabe577ab85e0624fedba
Voland24 <luka.mladenovic@elfak.rs>
2022-01-03 22:54:24 +0100

    Popravljeni Driver i upit za NEO4j

--- API/neo4jConnection.js
+++ API/neo4jConnection.js
@@ -1,6 +1,6 @@
 const neo4j = require('neo4j-driver');
 
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'pas123'));
+const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'pas123'),{ disableLosslessIntegers: true });
 
 const session = driver.session();
 
--- API/routes/neo4jProizvod.js
+++ API/routes/neo4jProizvod.js
@@ -7,7 +7,11 @@ router.get('/:nazivProizvoda', (req,res)=>{
     neo4jSession
             .run('MATCH (p:Proizvod{naziv:$nazivParam}) RETURN p', {nazivParam : nazivProizvoda})
             .then((result)=>{
-                res.send(result);
+                var nizProizvoda = [];
+                result.records.forEach(element => {
+                    nizProizvoda.push(element._fields[0].properties);
+                });
+                res.send(nizProizvoda);
             })
             .catch((err)=>{
                 res.status(500).send('Neo4j not working' + err);

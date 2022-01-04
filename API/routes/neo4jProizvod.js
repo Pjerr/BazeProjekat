5718bfc5ffaf911177fdc8c04ba37a87ca0e4e02
Voland24 <luka.mladenovic@elfak.rs>
2022-01-04 22:59:03 +0100

    Dve tabele imaju (skoro) full CRUD

--- API/routes/neo4jProizvod.js
+++ API/routes/neo4jProizvod.js
@@ -2,8 +2,8 @@ const express = require('express');
 const neo4jSession = require('../neo4jConnection');
 const router = express.Router();
 
-router.get('/:nazivProizvoda', (req,res)=>{
-    var nazivProizvoda = req.params.nazivProizvoda;
+router.get('/', (req,res)=>{
+    var nazivProizvoda = req.body.Naziv;
     neo4jSession
             .run('MATCH (p:Proizvod{naziv:$nazivParam}) RETURN p', {nazivParam : nazivProizvoda})
             .then((result)=>{
@@ -18,5 +18,38 @@ router.get('/:nazivProizvoda', (req,res)=>{
             });        
 })
 
+
+router.post('/dodajProizvod', (req,res)=>{
+    
+    var {brojKupovina,brojOcena,cena,kategorija,naziv,popust,tip,zbirOcena} = req.body;
+
+    
+    if(!naziv || popust < 0 || cena < 0 || brojKupovina < 0)
+        res.status(400).send('Bad request params');
+    
+    const addQuery = 'CREATE (p:Proizvod{brojKupovina : $brojKupovina,brojOcena : $brojOcena,cena : $cena,kategorija:$kategorija,naziv:$naziv,popust:$popust,tip:$tip,zbirOcena:$zbirOcena}) RETURN p';
+    neo4jSession
+                .run(addQuery,{brojKupovina,brojOcena,cena,kategorija,naziv,popust,tip,zbirOcena})
+                .then((result)=>{
+                    res.send(result);
+                })
+                .catch((err)=>{
+                    res.status(500).send('Neo4j not working' + err);
+                });
+})
+
+
+router.delete('/obrisiProizvod', (req,res)=>{
+    var naziv = req.body.Naziv;
+    var deleteQuery = 'MATCH (p:Proizvod{naziv:$naziv}) DELETE p';
+    neo4jSession
+                .run(deleteQuery, {naziv})
+                .then((result)=>{
+                    res.send(result);
+                })
+                .catch((err)=>{
+                    res.status(500).send('Neo4j not working' + err);
+                });
+})
 
 module.exports = router;
--- API/routes/proizvod_naziv.js
+++ API/routes/proizvod_naziv.js
@@ -1,18 +1,64 @@
 const express = require('express');
+const { copyFileSync } = require('fs');
 const cassandraClient = require('../cassandraConnect');
 const router = express.Router();
 
-router.get('/:naziv', (req,res)=>{
+
+//BITNO: Nema update ovde jer je naziv clustering key za ovu tabelu, ne moze da se menja
+
+//BODY SADRZI QUERY PARAMS
+router.get('/', (req,res)=>{
   var firstQuery = 'SELECT * FROM buyhub.proizvod_naziv WHERE kategorija = ? and tip = ? and naziv = ? ';
-    cassandraClient.execute(firstQuery, ['Racunarske komponente', 'SSD', req.params.naziv], (err, result)=>{
+    cassandraClient.execute(firstQuery, [req.body.Kategorija, req.body.Tip, req.body.Naziv], (err, result)=>{
         if(err){
             console.log('Unable to get data' + err);
         }
         else 
         {
+            console.log(req.body);
             res.status(200).send(result.rows)
         }
     })
 })
+
+//ISTO IZ BODY IDU PARAMS, SLATI SA /r/n za svaku stavku
+router.post('/dodajProizvod',(req,res)=>{
+
+    var addQuery = 'INSERT INTO buyhub.proizvod_naziv (kategorija, tip, naziv, cena, ocena, proizvodjac, opis, slika, popust) VALUES (?,?,?,?,?,?,?,?,?)';
+    cassandraClient.execute(addQuery,
+                            [req.body.Kategorija,req.body.Tip, req.body.Naziv, req.body.Cena, req.body.Ocena,req.body.Proizvodjac, req.body.Opis, req.body.Slika,req.body.Popust],
+                            { prepare : true },
+                            (err,result)=>{
+                                if(err){
+                                    console.log(req.body);
+                                    console.log('Unable to put data' + err);
+                                }
+                                else 
+                                {
+                                    console.log(req.body);
+                                    res.status(200).send(result);
+                                }
+                            })
+})
+
+//u body idu i kateogrija i tip i naziv
+router.delete('/obrisiProizvod', (req,res)=>{
+
+    var deleteQuery = 'DELETE FROM buyhub.proizvod_naziv WHERE kategorija = ? and tip = ? and naziv = ?';
+    cassandraClient.execute(deleteQuery, [req.body.Kategorija, req.body.Tip, req.body.Naziv], (err,result)=>{
+        if(err){
+            console.log(req.body);
+            console.log('Unable to delete data' + err);
+        }
+        else 
+        {
+            console.log(req.body);
+            res.status(200).send(result);
+        }
+    })
+
+})
+
+
 
 module.exports = router;

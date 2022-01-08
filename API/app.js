b03b6e957ef632e3948a632275b956c5e2438c81
Teodora Kalezic <teodora.kalezic@elfak.rs>
2022-01-08 22:56:05 +0100

    Preimenovani fajlovi

--- API/app.js
+++ API/app.js
@@ -1,27 +1,31 @@
 const express = require('express');
 
-const proizvodNazivRouter = require('./routes/proizvod_naziv');
-const ProizvodNeo4jRouter = require('./routes/neo4jProizvod');
-const popularniProizvodiRouter = require('./routes/popularniProizvodiCassandra');
-const proizvodjacRouter = require('./routes/proizvodjac');
-const RadnikNeo4jRouter = require('./routes/neo4jRadnik');
-const ProdavnicaNeo4jRouter = require('./routes/neo4jProdavnica');
-const transakcijaCassandraRouter = require('./routes/transakcijaCassandra')
-const korisnikNEORouter = require('./routes/korisnikNEO');
+const cas_proizvod_Router = require('./routes/cas_proizvod');
+const cas_proizvodjac_Router = require('./routes/cas_proizvodjac');
+const cas_transakcija_Router = require('./routes/cas_transakcija');
+const cas_popularno_Router = require('./routes/cas_popularno');
+
+const neo_proizvod_Router = require('./routes/neo_proizvod');
+const neo_prodavnica_Router = require('./routes/neo_prodavnica');
+const neo_korisnik_Router = require('./routes/neo_korisnik');
+const neo_radnik_Router = require('./routes/neo_radnik');
+
 
 
 const app = express();
 const APIRouter = express.Router();
 const rootRouter = express.Router();
-rootRouter.use('/proizvod_cassandra_tabele', proizvodNazivRouter);
-rootRouter.use('/neo4jProizvod', ProizvodNeo4jRouter);
-rootRouter.use('/popularniProizvodi', popularniProizvodiRouter);
-rootRouter.use('/proizvodjac', proizvodjacRouter);
-rootRouter.use('/neo4jRadnik', RadnikNeo4jRouter);
-rootRouter.use('/neo4jProdavnica', ProdavnicaNeo4jRouter);
-rootRouter.use('/transakcijaCassandra', transakcijaCassandraRouter);
+rootRouter.use('/proizvod_cassandra_tabele', cas_proizvod_Router);
+rootRouter.use('/proizvodjac', cas_proizvodjac_Router);
+rootRouter.use('/transakcijaCassandra', cas_transakcija_Router);
+rootRouter.use('/popularniProizvodi', cas_popularno_Router);
 
-rootRouter.use('/korisnik', korisnikNEORouter);
+rootRouter.use('/neo4jProizvod', neo_proizvod_Router);
+rootRouter.use('/neo4jProdavnica', neo_prodavnica_Router);
+rootRouter.use('/korisnik', neo_korisnik_Router);
+rootRouter.use('/neo4jRadnik', neo_radnik_Router);
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', rootRouter);
